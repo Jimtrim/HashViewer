84b825cf0aefe3c0d2d31d593c2b96ed32083ec1
jimtrim <jimfrodehoff@gmail.com>
2014-06-24 14:49:41 +0200

    OOP update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,73 @@
 // https://api.instagram.com/v1/tags/javielskerukm/media/recent?client_id=d81afea83c3f40b5a5485418e2a53aa7
 
-var CLIENT_ID = 'd81afea83c3f40b5a5485418e2a53aa7'
 
-var updateGallery = function() {
-	var tag = jQuery("input[id='tag-text']").val();
-	console.log(tag);
+var TagViewer = {
+	CLIENT_ID: 'd81afea83c3f40b5a5485418e2a53aa7',
+	next_url: '',
+	last_tag: '',
 
-	jQuery.ajax({
-		url: 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?client_id='+CLIENT_ID,
-		type: 'get',
-    	dataType: 'jsonp'
-	})
-	.done(function(res) {
+	createGalleryBlock: function(post) {
+		var image = post.images;
+		var user = post.user;
+		var caption = post.caption;
 
-		jQuery("#gallery").html('');
-		jQuery.each(res['data'], function(i, post) {
-			console.log(post);
-			var image = post['images']['low_resolution'];
-			var user = post['user'];
-			var caption = post['caption'];
+		var out = '<div class="image col-xs-6 col-md-4 col-lg-3">';
+		out += '<a href="'+post.link+'"><img src ="'+image.low_resolution.url+'" /></a><br/>';
+		out += '<em>Username: '+user.username+'</em><div class="clearfix></div></div>';
+		
+		return out;
+	}, 
 
-			var out = '<div class="image col-xs-6 col-md-4 col-lg-3">';
-			out += '<a href="'+post.link+'"><img src ="'+image.url+'" /></a><br/>';
-			// if (caption) out += '<span>'+caption.text+'</span><br/>';
-			out += '<em>Username: '+user.username+'</em><div class="clearfix></div></div>';
+	displayError: function(message) {
+		console.log(message);
+	},
 
-			jQuery("#gallery").append(out);
+	updateGallery: function(event) {
+		var tag = jQuery("input[id='tag-text']").val();
+		console.log(tag);
+		if (TagViewer.last_tag != tag) {
+			TagViewer.last_tag = tag;
+			TagViewer.next_url = undefined;
+			jQuery("#gallery").html('');
+		}
+
+		url = TagViewer.next_url || 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?client_id='+TagViewer.CLIENT_ID;
+		
+		jQuery.ajax({
+			url: url,
+			type: 'get',
+			dataType: 'jsonp'
+		})
+		.done(function(res) {
+			if (res.meta.code >= 400) { // if requests responds with HTTP error codes
+				TagViewer.displayError("ERROR: "+res.meta.error_message);
+			} else {
+				TagViewer.next_url = res.pagination.next_url;
+				jQuery.each(res.data, function(i, post) {
+					jQuery("#gallery").append(TagViewer.createGalleryBlock(post));
+				});
+				if (TagViewer.next_url) {
+					console.log(TagViewer.next_url);
+					jQuery("#more-btn").show();
+				} else {
+					jQuery("#more-btn").hide();
+				}
+			}
+
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
 		});
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
-	
+		
+		return this;
+	}
 };
 
-
-
 jQuery(document).ready(function($) {
-	$("button[id='tag-btn']").bind('click', updateGallery); 
+	$("button[id='tag-btn']").bind('click', TagViewer.updateGallery); 
 
 	$("input[id='tag-text']").keypress(function (e) {
         if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
