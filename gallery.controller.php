86189b888f5eb6985da9913a2e18b394e932a9c6
jimtrim <jimfrodehoff@gmail.com>
2014-07-02 21:35:30 +0200

    semicolon

--- gallery.controller.php
+++ gallery.controller.php
@@ -3,16 +3,16 @@
 	require 'vendor/autoload.php';
 
 	error_reporting( E_ALL );
-	if (isset($_GET['hashtag'])){
+	if ( isset($_GET['hashtag']) ){
 		$CLIENT_ID = "d81afea83c3f40b5a5485418e2a53aa7";
 
-		$tag =  "javielskerukm"; //$_GET['hashtag'];
+		$tag =  $_GET['hashtag'];
 		$uri	 = "https://api.instagram.com/v1/tags/" . $tag . "/media/recent?client_id=" . $CLIENT_ID;
 		$response = Httpful\Request::get($uri)->send();
 
 		echo("".$response);
 	} else {
-		echo("{}")
+		echo("{}");
 	}
 
 
