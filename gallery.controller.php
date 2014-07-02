d75e1cbed5f3f531a0d3a519fe4c55cae94f0e17
jimtrim <jimfrodehoff@gmail.com>
2014-07-02 21:07:03 +0200

    use get parameter

--- gallery.controller.php
+++ gallery.controller.php
@@ -3,13 +3,18 @@
 	require 'vendor/autoload.php';
 
 	error_reporting( E_ALL );
-	$CLIENT_ID = "d81afea83c3f40b5a5485418e2a53aa7";
+	if (isset($_GET['hashtag'])){
+		$CLIENT_ID = "d81afea83c3f40b5a5485418e2a53aa7";
 
-	$tag =  "javielskerukm"; //$_GET['hashtag'];
-	$uri	 = "https://api.instagram.com/v1/tags/" . $tag . "/media/recent?client_id=" . $CLIENT_ID;
-	$response = Httpful\Request::get($uri)->send();
+		$tag =  "javielskerukm"; //$_GET['hashtag'];
+		$uri	 = "https://api.instagram.com/v1/tags/" . $tag . "/media/recent?client_id=" . $CLIENT_ID;
+		$response = Httpful\Request::get($uri)->send();
 
-	echo("".$response);
+		echo("".$response);
+	} else {
+		echo("{}")
+	}
+
 
 ?>
 
